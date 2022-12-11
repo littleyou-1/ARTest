6b1763293460c33dde113ab5c4cb206577c7265a
littleyou-1 <53014164+littleyou-1@users.noreply.github.com>
2022-12-11 10:31:44 +0200

    Add files via upload

---
+++ webAnimal/script.js
@@ -0,0 +1,33 @@
+let butFront = document.querySelector('#ar-buttonFront');
+let butBack = document.querySelector('#ar-button');
+let FrontPage = document.querySelector('#LoadingPage');
+
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+
+    
+
+  } else {
+    progressBar.classList.remove('hide');
+    if (event.detail.totalProgress === 0) {
+      event.target.querySelector('.center-pre-prompt').classList.add('hide');
+
+    }
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
+butFront.addEventListener('click', function() {
+  butBack.click();
+  alert("Hello");
+  FrontPage.style.display="none";
+});
+/* document.querySelector('model-viewer').addEventListener('progress', onProgress);
+butFront.addEventListener('click', function() {
+  butBack.click();
+}); */
+
