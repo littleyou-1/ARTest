2933dbcc21603c0f3b414ed8805c64974de51538
littleyou-1 <53014164+littleyou-1@users.noreply.github.com>
2022-12-11 12:04:58 +0200

    Update script.js

--- webAnimal/script.js
+++ webAnimal/script.js
@@ -24,7 +24,6 @@ const onProgress = (event) => {
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
 butFront.addEventListener('click', function() {
   butBack.click();
-  FrontPage.style.display = 'none';
 });
 document.addEventListener('DOMContentLoaded', (event) => {
   butFront.addEventListener('click', function() {
